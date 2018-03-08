0e96476affbd3da8300ff2d475ed579f82f351c4
Oliver Balfour <leolalor1@gmail.com>
2018-03-08 14:04:12 +1100

    Made basic (ugly) build process with gulp + gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,12 @@ const gulp = require('gulp');
 const marked = require('marked');
 
 gulp.task('default', () => {
-	let parsed = marked(fs.readFileSync(path.join(__dirname, 'index.md')));
-	let template = fs.readFileSync(path.join(__dirname, 'template.html'));
-	template.replace('<INCLUDE/>', parsed);
-	fs.writeFileSync(path.join(__dirname, 'index.html'), template);
+	let parsed = marked(fs.readFileSync(path.join(__dirname, 'index.md'), 'utf-8')),
+		template = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8'),
+		index = template.replace('<INCLUDE/>', parsed);
+	fs.writeFileSync(path.join(__dirname, 'index.html'), index);
+});
+
+gulp.task('watch', () => {
+	gulp.watch('index.md', ['default']);
 });
