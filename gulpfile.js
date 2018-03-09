64303d51acd5891be85d3f7243f8f5fc4e214c98
Oliver Balfour <leolalor1@gmail.com>
2018-03-09 12:47:50 +1100

    Added SCSS to the build process

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,23 @@ const path = require('path');
 
 const gulp = require('gulp');
 const marked = require('marked');
+const sass = require('gulp-sass');
 
-gulp.task('default', () => {
+gulp.task('default', ['scss', 'md']);
+
+gulp.task('md', () => {
 	let parsed = marked(fs.readFileSync(path.join(__dirname, 'index.md'), 'utf-8')),
 		template = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8'),
 		index = template.replace('<INCLUDE/>', parsed);
 	fs.writeFileSync(path.join(__dirname, 'index.html'), index);
 });
 
+gulp.task('scss', () =>
+	gulp.src('index.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('.'))
+);
+
 gulp.task('watch', () => {
-	gulp.watch('index.md', ['default']);
+	gulp.watch('index.*', ['default']);
 });
