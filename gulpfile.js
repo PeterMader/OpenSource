b80be2a3d5357fffa51be1176ce4b237c06cba02
Oliver Balfour <leolalor1@gmail.com>
2018-03-11 18:08:38 +1100

    Cleaned up, minification added to build process, autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,14 @@ const fs = require('fs');
 const path = require('path');
 
 const gulp = require('gulp');
+
 const marked = require('marked');
+const htmlmin = require('html-minifier').minify;
+
 const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 gulp.task('default', ['scss', 'md']);
 
@@ -12,15 +18,22 @@ gulp.task('md', () =>
 	fs.writeFileSync(
 		path.join(__dirname, 'index.html'),
 		// marked doesn't parse the DOCTYPE declaration properly, insert here instead
-		'<!DOCTYPE html>\n' + marked(
+		htmlmin(( '<!DOCTYPE html>\n' + marked(
 			fs.readFileSync(path.join(__dirname, 'main.html'), 'utf-8')
-		)
+		)), {
+			collapseWhitespace: true,
+			removeComments: true
+		})
 	)
 );
 
 gulp.task('scss', () =>
 	gulp.src('index.scss')
 		.pipe(sass())
+		.pipe(postcss([
+			autoprefixer({browsers: ['last 2 versions', '> 1%']}),
+			cssnano()
+		]))
 		.pipe(gulp.dest('.'))
 );
 
