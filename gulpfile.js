cd84cef1ef6f9952a9db65ac3d7b794e08387928
Oliver Balfour <leolalor1@gmail.com>
2018-03-16 09:42:25 +1100

    f*ck fsevents

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,15 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
+// const imagemin = require('imagemin');
+// const imageminJpegtran = require('imagemin-jpegtran');
+// const imageminPngquant = require('imagemin-pngquant');
+// const imagemingGifsicle = require('imagemin-gifsicle');
+
 const browserSync = require('browser-sync').create();
 
 
+// gulp.task('default', ['html', 'css', 'image']);
 gulp.task('default', ['html', 'css']);
 
 gulp.task('html', done => {
@@ -42,6 +48,26 @@ gulp.task('css', () =>
 		.pipe(browserSync.stream())
 );
 
+// fuck fsevents
+// that stupid module makes installation for imagemin hang on linux
+
+// gulp.task('image', () => {
+// 	imagemin(['raw-images/*.{jpg,png,gif}'], 'images', {
+// 		plugins: [
+// 			imageminJpegtran(),
+// 			imageminPngquant({quality: '65-80'}),
+// 			imagemingGifsicle()
+// 		]
+// 	}).then(files => {
+// 		files.forEach(obj =>
+// 			fs.writeFileSync(
+// 				path.join(__dirname, obj.path),
+// 				data
+// 			)
+// 		)
+// 	});
+// });
+
 gulp.task('reload', done => {
 	browserSync.reload();
 	done();
@@ -51,6 +77,7 @@ gulp.task('watch', () => {
 	gulp.watch('main.html', ['html', 'reload']);
 	gulp.watch('*.scss', ['css']);
 	gulp.watch('index.js', ['reload']);
+	// gulp.watch('raw-images/*', ['image']);
 
 	browserSync.init({
 		server: {
