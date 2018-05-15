ec5485633fdbe65830673d1df6f8d253813876b1
Oliver Balfour <leolalor1@gmail.com>
2018-05-15 20:26:52 +1000

    WIP (gulp broke on laptop :-/)

---
+++ de/index.js
@@ -0,0 +1,60 @@
+
+// corporation name slideshow
+
+var slideshows = Array.from(
+	document.getElementsByClassName('company-slideshow')
+),
+	corporations = [
+		'Google',
+		'Microsoft',
+		'Facebook',
+		'Apple'
+	],
+	index = 0;
+
+setInterval(function () {
+	index++;
+	if (index === corporations.length) index = 0;
+	for (var i = 0; i < slideshows.length; i++) {
+		slideshows[i].innerText = corporations[index];
+	}
+}, 2000);
+
+var highlighted = 0;
+
+function setHash (hash) {
+	if (location.hash !== '#' + hash) {
+		if (history.pushState) {
+			history.pushState(null, null, '#'+hash);
+		} else {
+			location.hash = '#'+hash;
+		}
+	}
+}
+setHash('/proprietary-software/');
+document.querySelector('header nav a').classList.add('active');
+
+document.addEventListener('scroll', function () {
+	if (document.getElementById('/more-info/').getBoundingClientRect().y < 100) {
+		highlighted = 3;
+		setHash('/more-info/');
+		document.title = "More Information";
+	} else if (document.getElementById('/free-software-library/').getBoundingClientRect().y < 100) {
+		highlighted = 2;
+		setHash('/free-software-library/');
+		document.title = "Free and Open Source Software Library";
+	} else if (document.getElementById('/open-source/').getBoundingClientRect().y < 100) {
+		highlighted = 1;
+		setHash('/open-source/');
+		document.title = "The Advantages of Free Software";
+	} else if (document.getElementById('/proprietary-software/').getBoundingClientRect().y < 100) {
+		highlighted = 0;
+		setHash('/proprietary-software/');
+		document.title = "The Problem With Proprietary Software";
+	}
+
+	for (el of Array.from(document.querySelectorAll('header nav a'))) {
+		el.classList.remove('active');
+	}
+	document.querySelectorAll('header nav a')[highlighted].classList.add('active');
+})
--- gulpfile.js
+++ gulpfile.js
@@ -12,28 +12,26 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
-// const imagemin = require('imagemin');
-// const imageminJpegtran = require('imagemin-jpegtran');
-// const imageminPngquant = require('imagemin-pngquant');
-// const imagemingGifsicle = require('imagemin-gifsicle');
-
 const browserSync = require('browser-sync').create();
 
 
-// gulp.task('default', ['html', 'css', 'image']);
 gulp.task('default', ['html', 'css']);
 
 gulp.task('html', done => {
-	fs.writeFileSync(
-		path.join(__dirname, 'index.html'),
-		// marked doesn't parse the DOCTYPE declaration properly, insert here instead
-		htmlmin(( '<!DOCTYPE html>\n' + marked(
-			fs.readFileSync(path.join(__dirname, 'main.html'), 'utf-8')
-		)), {
-			collapseWhitespace: true,
-			removeComments: true
-		})
-	);
+	const processHTML = (source, dest) => {
+		fs.writeFileSync(
+			path.join(__dirname, dest),
+			// marked doesn't parse the DOCTYPE declaration properly, insert here instead
+			htmlmin(( '<!DOCTYPE html>\n' + marked(
+				fs.readFileSync(path.join(__dirname, source), 'utf-8')
+			)), {
+				collapseWhitespace: true,
+				removeComments: true
+			})
+		);
+	}
+	processHTML('main.html', 'index.html');
+	processHTML('de/main.html', 'de/index.html');
 	done();
 });
 
@@ -48,26 +46,6 @@ gulp.task('css', () =>
 		.pipe(browserSync.stream())
 );
 
-// fuck fsevents
-// that stupid module makes installation for imagemin hang on linux
-
-// gulp.task('image', () => {
-// 	imagemin(['raw-images/*.{jpg,png,gif}'], 'images', {
-// 		plugins: [
-// 			imageminJpegtran(),
-// 			imageminPngquant({quality: '65-80'}),
-// 			imagemingGifsicle()
-// 		]
-// 	}).then(files => {
-// 		files.forEach(obj =>
-// 			fs.writeFileSync(
-// 				path.join(__dirname, obj.path),
-// 				data
-// 			)
-// 		)
-// 	});
-// });
-
 gulp.task('reload', done => {
 	browserSync.reload();
 	done();
@@ -75,9 +53,9 @@ gulp.task('reload', done => {
 
 gulp.task('watch', () => {
 	gulp.watch('main.html', ['html', 'reload']);
+	gulp.watch('de/main.html', ['html', 'reload']);
 	gulp.watch('*.scss', ['css']);
 	gulp.watch('index.js', ['reload']);
-	// gulp.watch('raw-images/*', ['image']);
 
 	browserSync.init({
 		server: {
